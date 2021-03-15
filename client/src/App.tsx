3928d1549026d86b480764a32bde428067af76a3
JunYoung <vnslt0152@gmail.com>
2021-03-15 16:04:04 +0900

    update: App.tsx update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import React from 'react';
-import { CookiesProvider } from 'react-cookie';
 
 const App: React.FC = () => {
   return <h1>test</h1>;
